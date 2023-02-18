ae9920acf1e764d887919b09e2d2f25cc6a71345
Eric Thomas <eric@ericthomas.ca>
2023-02-18 14:20:50 -0330

    get all posts from db

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate diesel;
+
+#[macro_use]
+extern crate serde_derive;
+
+extern crate dotenv;
+
+mod db;
+use db::*;
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/api/posts")]
+async fn get_all_posts() -> impl Responder {
+    let posts = get_posts();
+    HttpResponse::Ok().json(posts)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(get_all_posts))
+            .bind(("0.0.0.0", 8088))?
+            .run()
+            .await
 }
